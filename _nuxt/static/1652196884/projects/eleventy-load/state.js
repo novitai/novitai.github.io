907a72a70b51b45d01561eb0ef6e3a55a4b09a3a
emingenc <emingenc@users.noreply.github.com>
2022-05-10 15:51:44 +0000

    deploy: 4c88e4d3acb6753d357f79ac6ac25bad10479e0c

--- _nuxt/static/1652196884/projects/eleventy-load/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652196884",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Feleventy-load\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
