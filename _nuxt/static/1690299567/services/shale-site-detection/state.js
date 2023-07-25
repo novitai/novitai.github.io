f53c9af9875cfd038ac3eb072a0782ffbcf35465
elifdoganay1 <elifdoganay1@users.noreply.github.com>
2023-07-25 15:39:31 +0000

    deploy: 5631d821c92491092bac8992930343a32223981a

---
+++ _nuxt/static/1690299567/services/shale-site-detection/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1690299567",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Fshale-site-detection\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
