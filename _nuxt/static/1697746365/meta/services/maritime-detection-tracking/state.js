6fdd9cf00e39f5971aea1d2e46ddeb333bfd03d7
elifdoganay1 <elifdoganay1@users.noreply.github.com>
2023-11-01 08:58:14 +0000

    deploy: bca0000cfdbd59d2c8f157514008a696a9207a7f

--- _nuxt/static/1697746365/meta/services/maritime-detection-tracking/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1697746365",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmeta\u002Fservices\u002Fmaritime-detection-tracking\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
